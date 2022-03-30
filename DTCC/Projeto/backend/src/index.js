1ab5a47b71b31cf2590733ed6a574aa88a386278
Sérgio de Siqueira Silva <sergiodsiqueira@yahoo.com.br>
2022-03-30 19:39:45 -0300

    Initial TCC Project

---
+++ DTCC/Projeto/backend/src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+const port = 3000;
+
+var dataHora = new Date();
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+app.get("/", (req, res) =>
+  res.json({ info: "Bem-vindo a API e-Clinic " + dataHora })
+);
+
+app.listen(port, () => console.log(`API e-Clinic escutando a porta:${port}`));
