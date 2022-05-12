e2219d37e2b5eb29acd93d32d3de7f16e5a8b763
Sérgio de Siqueira Silva <sergiodsiqueira@yahoo.com.br>
2022-05-12 14:36:59 -0300

    POC Login

--- DTCC/Projeto/frontend/lib/src/view/eclinic.view.login.dart
+++ DTCC/Projeto/frontend/lib/src/view/eclinic.view.login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -9,7 +10,25 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: (Center(child: TextField())),
+      body: (Center(
+          child: Row(
+        children: [
+          Container(
+              margin: const EdgeInsets.all(2.0),
+              color: Colors.amber[600],
+              width: 500.0,
+              height: 500.0,
+              child: (Image.network(
+                  'https://media.istockphoto.com/vectors/smartphone-screen-with-female-therapist-on-chat-vector-id1253363497'))),
+          Container(
+            margin: const EdgeInsets.all(2.0),
+            color: Colors.red[600],
+            width: 500.0,
+            height: 500.0,
+            child: (Text('Inputs e Labels de Login')),
+          ),
+        ],
+      ))),
     );
   }
 }
